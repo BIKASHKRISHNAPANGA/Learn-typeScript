ca64c02f7eba79c5fadcff35c7dab153600c9b15
bikash krishna panga <developerbikash16@gmail.com>
2025-03-11 23:50:10 +0530

    type narrowing

--- pure-ts/src/type-Narrowing.ts
+++ pure-ts/src/type-Narrowing.ts
@@ -12,3 +12,37 @@ function provideId(id:string| null){
     }
     id?.toLowerCase()
 }
+
+function PrintAll(strs:string|string[]|null){
+    if(strs){
+        if(Array.isArray(strs)){
+            console.log("its array and it has")
+            strs.forEach(element => {console.log(element)})
+        }
+        else if(typeof strs==="string"){
+            console.log(strs+"is a string")
+        }
+    }
+}
+
+
+interface user{
+    name: string;
+    age: number;
+    email:string
+}
+
+interface admin{
+    name: string;
+    isAdmin:boolean
+}
+
+function isAdmin(account :user|admin){
+if("isAdmin" in account && account.isAdmin){
+console.log(account.name+"has a accaount")
+}else {
+    console.log(account.name + " is a regular user");
+}
+}
+
+// its just narrowing the type to use it properly
